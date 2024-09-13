c8aa049583b7f951e788c1166a346eb81a3c10de
andrejbarysnikov <andrey1andrey@narod.ru>
2024-09-13 22:04:31 +0300

    1

--- MyObject/Program.cs
+++ MyObject/Program.cs
@@ -7,8 +7,8 @@
             AnyObject anyObject = new();
             int hashCode = anyObject.GetHashCode();
             bool equals = anyObject.Equals(new object());
-            Type type = anyObject.GetType();
             string str = anyObject.ToString();
+            Type type = anyObject.GetType();
             bool equalsStatic = AnyObject.Equals(anyObject, new AnyObject());
             bool referenceEquals = AnyObject.ReferenceEquals(anyObject, new object());
         }
